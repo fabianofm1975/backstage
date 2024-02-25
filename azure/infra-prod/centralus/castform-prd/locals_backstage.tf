7ef6637648344ee0f4f64f8123319fe0ddf1eaaf
Fabiano Furtado de Melo <fabianofm1975@gmail.com>
2024-02-24 21:39:12 -0300

    feat: create database -  castform

---
+++ azure/infra-prod/centralus/castform-prd/locals_backstage.tf
@@ -0,0 +1,20 @@
+locals {
+  backstage = {
+      env             = "prd"
+      subscription_name = "INFRA-PROD"
+      resource_group_name            = "castform-prd"
+      resource_group_location = "centralus"
+      postgres_instance_name                         = "castform-prd-db"
+      postgres_instance_version                      = 14
+      postgres_instance_storage_mb                   = 49152
+      postgres_instance_sku_name                     = "GP_Standard_D2s_v3"
+      enable_replica_readonly               = "true" == "true" ? true : false
+
+      tags = {
+        "env"         =  "prd" == "stg" ? "staging" : "prd" == "sdx" ? "sandbox" : "production" 
+        "cost-center" = "860290033"
+        "project"     = "castform"
+        "owner"       = "group:default/infrastructure"
+      }
+  }
+}
