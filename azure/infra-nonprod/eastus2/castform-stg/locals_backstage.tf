1784384d3c56a61537a4370b4629c12cc6dfe11c
Fabiano Furtado de Melo <fabianofm1975@gmail.com>
2024-02-25 13:37:45 -0300

    feat: create database -  castform

---
+++ azure/infra-nonprod/eastus2/castform-stg/locals_backstage.tf
@@ -0,0 +1,23 @@
+locals {
+  backstage = {
+      env             = "stg"
+      subscription_name = "INFRA-NONPROD"
+      resource_group_name            = "castform-stg"
+      resource_group_location = "eastus2"
+      postgres_instance_name                         = "castform"
+      postgres_instance_version                      = 14
+      postgres_instance_storage_mb                   = 32768
+      postgres_instance_sku_name                     = "B_Standard_B1ms"
+      enable_replica_readonly               = "true" == "true" ? true : false
+      postgres_replica_sku_name = "B_Standard_B1ms"
+
+      tags = {
+        "env"         =  "stg" == "stg" ? "staging" : "prd" == "sdx" ? "sandbox" : "production" 
+        "cost-center" = "860290033"
+        "project"     = "castform"
+        "owner"       = "group:default/team-b"
+      }
+
+     app_user = "castform"
+  }
+}
