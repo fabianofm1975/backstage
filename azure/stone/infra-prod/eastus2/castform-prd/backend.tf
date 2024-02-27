05b743204a028634b542c7836d4d3a07f1c922a2
Fabiano Furtado de Melo <fabianofm1975@gmail.com>
2024-02-27 10:27:47 -0300

    feat: create database -  castform

---
+++ azure/stone/infra-prod/eastus2/castform-prd/backend.tf
@@ -0,0 +1,12 @@
+terraform {
+  backend "s3" {
+    encrypt        = "true"
+    bucket         = "foundation-platform-terraform-state-tools-prd"
+    key            = "data-persistence-terraform-deploys/azure/stone/postgres/INFRA-PROD/castform-prd/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "atlantis-eks-stone-tools-prd"
+    role_arn       = "arn:aws:iam::322745845743:role/atlantis-eks-bucket-policy-stone-tools-prd"
+    session_name   = "data-persistence-terraform-deploys"
+  }
+}
+
