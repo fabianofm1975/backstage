ea654d72d1b793ec7d2fc247db6a3789ecc22556
Fabiano Furtado de Melo <fabianofm1975@gmail.com>
2024-03-03 02:28:15 -0300

    feat: create database -  castform

---
+++ aws/acquiring-stg/castform-stg/backstage_parameters.tf
@@ -0,0 +1,20 @@
+locals {
+  backstage = {
+    environment       = "stg"
+    aws_account_name  = "acquiring-stg"
+    engine_version    = "14"
+    family            = "postgres14"
+    instance_name     = "castform-stg"
+    instance_class    = "db.t4g.small"
+    allocated_storage = 10
+    aws_region        = "us-east-1"
+
+    enable_replica_readonly               = try("","false") == "Sim" ? true : false
+    postgres_replica_sku_name = "db.t4g.small"
+
+    env         =  "stg" == "stg" ? "staging" : "prd" == "sdx" ? "sandbox" : "production" 
+    cost-center = "860290035"
+    project     = "castform"
+    owner     = "group:default/team-d"
+  }
+}
